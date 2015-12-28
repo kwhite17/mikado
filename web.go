806ac4fce48b94b5fadc9581afcf5a67d1340668
kwhite17 <kevinwhite1710@gmail.com>
2015-12-28 16:50:22 -0500

    updated scrolling functionality with some success

---
+++ web.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+type wsHandler struct {
+	connections map[*websocket.Conn]bool
+}
+
+var upgrader = websocket.Upgrader{ReadBufferSize: 1024, WriteBufferSize: 1024}
+
+func (handler wsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	handler.connections[conn] = true
+	sendMessage(handler.connections, conn)
+}
+
+func sendMessage(conns map[*websocket.Conn]bool, source *websocket.Conn) {
+	for {
+		messageType, message, err := source.ReadMessage()
+		if err != nil {
+			source.Close()
+			log.Println(err)
+			return
+		}
+		for k := range conns {
+			if source != k {
+				k.WriteMessage(messageType, message)
+			}
+		}
+	}
+}
+
+func main() {
+	var connections = make(map[*websocket.Conn]bool)
+	handler := wsHandler{connections: connections}
+	http.Handle("/", http.FileServer(http.Dir("./app")))
+	http.Handle("/socket", handler)
+	log.Fatalf("%s", http.ListenAndServe(":4000", nil))
+}
