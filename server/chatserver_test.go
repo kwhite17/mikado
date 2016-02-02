e4a39b6b9dc3bde988402d6437a1297d007ad922
kwhite17 <kevinwhite1710@gmail.com>
2016-02-01 22:04:56 -0500

    Fixed dependency information

--- server/chatserver_test.go
+++ server/chatserver_test.go
@@ -1,30 +1,28 @@
-package test
+package server
 
 import (
 	"net/http"
 	"reflect"
 	"testing"
 	"time"
-
-	"github.com/kwhite17/mikado/server"
 )
 
 func TestBuildServerReturnsServerWithCorrectReadTimeout(t *testing.T) {
-	customServer := server.BuildChatServer("")
+	customServer := BuildChatServer("")
 	if customServer.ReadTimeout != 36000*time.Second {
 		t.Fatalf("Expected Read Timeout: 36000, Actual Read Timeout: %d", customServer.ReadTimeout)
 	}
 }
 
 func TestBuildServerReturnsServerWithCorrectWriteTimeout(t *testing.T) {
-	customServer := server.BuildChatServer("")
+	customServer := BuildChatServer("")
 	if customServer.WriteTimeout != 36000*time.Second {
 		t.Fatalf("Expected Write Timeout: 36000, Actual Read Timeout: %d", customServer.WriteTimeout)
 	}
 }
 
 func TestBuildServeMuxReturnsHandlerWithServeMuxType(t *testing.T) {
-	handler := server.BuildServeMux()
+	handler := BuildServeMux()
 	actual := reflect.TypeOf(handler).String()
 	expected := reflect.TypeOf(&http.ServeMux{}).String()
 	if expected != actual {
@@ -33,23 +31,23 @@ func TestBuildServeMuxReturnsHandlerWithServeMuxType(t *testing.T) {
 }
 
 func TestBuildHandlerReturnsHandlerWithSocketHandlerType(t *testing.T) {
-	handler := server.BuildHandler()
+	handler := BuildHandler()
 	actual := reflect.TypeOf(handler).String()
-	expected := reflect.TypeOf(&server.SocketHandler{}).String()
+	expected := reflect.TypeOf(&SocketHandler{}).String()
 	if expected != actual {
 		t.Fatalf("Expected: %s, Actual: %s", expected, actual)
 	}
 }
 
 func TestBuildHandlerReturnsHandlerWithConnectionsMap(t *testing.T) {
-	handler := server.BuildHandler()
+	handler := BuildHandler()
 	if handler.Connections == nil {
 		t.Fatal("No map exists to manage connections!")
 	}
 }
 
 func TestBuildServerReturnsServerWithCorrectHandler(t *testing.T) {
-	customServer := server.BuildChatServer("")
+	customServer := BuildChatServer("")
 	if customServer.Handler == nil {
 		t.Fatal("Server is missing appropriate handler!")
 	}
@@ -57,7 +55,7 @@ func TestBuildServerReturnsServerWithCorrectHandler(t *testing.T) {
 
 func TestBuildServerReturnsServerWithCorrectAddress(t *testing.T) {
 	expected := ":4000"
-	customServer := server.BuildChatServer(expected)
+	customServer := BuildChatServer(expected)
 	if customServer.Addr != ":4000" {
 		t.Fatalf("Wrong Port Number! Expected: %s, Actual: %s", expected, customServer.Addr)
 	}
